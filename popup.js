19e3613ecc3ab512cf0bb945cb273653f725df5f
AakashAxe <97781384+AakashAxe@users.noreply.github.com>
2022-05-21 05:43:32 +1000

    GREAT PROGRESS
    Was able to allow the button to take me to google.com - Thanks Meherwan!

--- popup.js
+++ popup.js
@@ -5,3 +5,11 @@ chrome.storage.sync.get("color", ({color}) => {
     changeColor.style.backgroundColor = color;
 
 })
+
+ let Butt = document.getElementById("changeColor");
+        Butt.addEventListener("click", newWindowFunc );
+
+        function newWindowFunc() {
+            window.open('http://google.com','_newtab') 
+
+        }
