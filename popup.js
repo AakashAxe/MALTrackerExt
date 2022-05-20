d7e823ed1b88911da6f67343595885dc78393ae6
AakashAxe <97781384+AakashAxe@users.noreply.github.com>
2022-05-21 05:12:08 +1000

    New Color Button Added
    Copied code from the dev extension link, and added a green button.
    - ahref on button didnt work

---
+++ popup.js
@@ -0,0 +1,7 @@
+// Initialise button with user's preferred colour
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({color}) => {
+    changeColor.style.backgroundColor = color;
+
+})
